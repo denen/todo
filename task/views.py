6aa7b568f98b7a689d09f16193df6d390a15e086
mitatamishiro <mitatamishiro@gmail.com>
2020-12-14 21:15:33 +0900

    投稿可能。ajaxをつけてない

---
+++ task/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.views.generic import View
+
+
+from .models import Task
+from .forms import TaskForm
+
+class TaskList(View):
+    def get(self, request):
+        form = TaskForm()
+        tasks = Task.objects.all()
+        return render(request, 'task/task_list.html', context={'form':form, 'tasks':tasks})
+
+    def post(self, request):
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            new_task = form.save()
+            return redirect('task_list_url')
+        else:
+            return redirect('task_list_url')
