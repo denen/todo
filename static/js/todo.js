11485ade89abb09d45f9ebe1d7f25baec0b164eb
mitatamishiro <mitatamishiro@gmail.com>
2020-12-14 22:34:51 +0900

    追加までpostでできた やっぱりajaxのバージョンが悪かったらしい 動画では3.4.1。最初に入れた4以上だとsuccessが使えないのかも？

--- static/js/todo.js
+++ static/js/todo.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
             data: serializeData,
             type: 'post',
             success: function(response){
-                $("#taskList").append("足しました")
+                $("#taskList").append('<div class="card mb-1"><div class="card-body">' + response.task.title + '<button type="button" class="close float-right"><span aria-hidden="true">&times;</span></button></div></div>')
             }
         })
 
--- task/views.py
+++ task/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 from .models import Task
 from .forms import TaskForm
+
 
 class TaskList(View):
     def get(self, request):
@@ -16,6 +19,6 @@ class TaskList(View):
 
         if form.is_valid():
             new_task = form.save()
-            return redirect('task_list_url')
+            return JsonResponse({'task': model_to_dict(new_task)}, status=200)
         else:
             return redirect('task_list_url')
