f41c331a9475c1180b957d2b17112e5fc39f2ddc
mitatamishiro <mitatamishiro@gmail.com>
2020-12-14 22:47:34 +0900

    div class="card"にdata-idというプロパティ名 <v class="card mb-1" data-id="' + response.task.id '"></div>" 見たこと無いけど要素のプロパティ名勝手につけていいの？

--- static/js/todo.js
+++ static/js/todo.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
             data: serializeData,
             type: 'post',
             success: function(response){
-                $("#taskList").append('<div class="card mb-1"><div class="card-body">' + response.task.title + '<button type="button" class="close float-right"><span aria-hidden="true">&times;</span></button></div></div>')
+                $("#taskList").append('<div class="card mb-1" id="taskCard" data-id="' + response.task.id + '"><div class="card-body">' + response.task.title + '<button type="button" class="close float-right"><span aria-hidden="true">&times;</span></button></div></div>')
             }
         })
 
